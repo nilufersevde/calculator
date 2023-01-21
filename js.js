e97e2d307171b0f52cfa9dd27838dc54bb767e1d
Nilufer <n.sevde@hotmail.com>
2023-01-21 17:19:10 +0300

    add eventListener to number buttons

--- js.js
+++ js.js
@@ -1,18 +1,49 @@
-function add(num1,num2) {
-      return num1+num2;
-};
+function operate(operator,num1,num2) {
+    num1=Number(num1)
+    num2=Number(num2)
+    switch(operator) {
+        case '+':
+            return num1+num2
+        case '-':
+            return num1-num2 
+        case '*':
+            return num1*num2
+        case '/':
+            return num1/num2
+        default:
+             return null      
+        }
+    }
 
-function substract(num1,num2) {
-  return num1-num2;
-};
 
-function multiply(num1,num2) {
-      num1*num2;
-      return num1*num2;
-  };
 
-function divide(num1,num2) {
-      return num1/num2;
-  };
+//evaluates the result when click to the equals button
+document.getElementsByClassName("equal").addEventListener('click',evaluate)
+function evaluate {
+    let firstnum=
+    let secondnum=
+    let operator=
+    operate(operator,firstnum,secondnum)
+}
+
+function calloperator(operator) {
+
+}
+
+
+
+
+
+//displaying the content of clisked buttons on the screen 
+const currentScreen=document.getElementById("currentScreen");
+const numberButtons=document.querySelectorAll(".number");
+numberButtons.forEach((button) => 
+    button.addEventListener('click', () => 
+    currentScreen.textContent+=button.textContent)
+)
+
+
+
+
 
 
