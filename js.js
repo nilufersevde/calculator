480b1a71f8410816624083bea29bd60a9166d399
Nilufer <n.sevde@hotmail.com>
2023-01-21 09:18:08 +0300

    add operator functions

---
+++ js.js
@@ -0,0 +1,18 @@
+function add(num1,num2) {
+      return num1+num2;
+};
+
+function substract(num1,num2) {
+  return num1-num2;
+};
+
+function multiply(num1,num2) {
+      num1*num2;
+      return num1*num2;
+  };
+
+function divide(num1,num2) {
+      return num1/num2;
+  };
+
+
