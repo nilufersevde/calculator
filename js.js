e24fed88bdb5055870b51ce0556c657b223f73aa
Nilufer <n.sevde@hotmail.com>
2023-01-22 12:55:22 +0300

    add equals, reset , delete functions

--- js.js
+++ js.js
@@ -1,38 +1,21 @@
+
 function operate(operator,num1,num2) {
     num1=Number(num1)
     num2=Number(num2)
+    lastOperation.textContent+= "="
     switch(operator) {
         case '+':
             return num1+num2
         case '-':
             return num1-num2 
-        case '*':
+        case 'x':
             return num1*num2
-        case '/':
+        case '÷':
             return num1/num2
         default:
              return null      
         }
     }
-
-
-
-//evaluates the result when click to the equals button
-document.getElementsByClassName("equal").addEventListener('click',evaluate)
-function evaluate {
-    let firstnum=
-    let secondnum=
-    let operator=
-    operate(operator,firstnum,secondnum)
-}
-
-function calloperator(operator) {
-
-}
-
-
-
-
 
 //displaying the content of clisked buttons on the screen 
 const currentScreen=document.getElementById("currentScreen");
@@ -41,6 +24,47 @@ numberButtons.forEach((button) =>
     button.addEventListener('click', () => 
     currentScreen.textContent+=button.textContent)
 )
+
+const lastOperation=document.getElementById("lastOperation");
+const alloperators= document.querySelectorAll(".operator");
+alloperators.forEach((button) => 
+    button.addEventListener('click', () => {
+    lastOperation.textContent=currentScreen.textContent+button.textContent;
+    currentScreen.innerHTML="";}
+    ))
+
+//evaluates the result when click to the equals button
+document.getElementById("equals").addEventListener('click',evaluate);
+function evaluate() {
+    let lastOperationText=lastOperation.textContent;
+    let firstnum = Number(lastOperationText.slice(0, lastOperationText.length - 1));
+    let secondnum = Number(currentScreen.textContent);
+    lastOperation.textContent+=currentScreen.textContent;
+    currentScreen.innerHTML =""
+    let operator =lastOperationText.slice(-1);
+    let result=operate(operator,firstnum,secondnum);
+    currentScreen.textContent=result;
+}
+
+document.getElementById("clear").addEventListener('click',reset);
+function reset() {
+    currentScreen.innerHTML ="";
+    lastOperation.innerHTML ="";
+}
+
+document.getElementById("delete").addEventListener('click',deletefunc);
+function deletefunc() {
+    currentScreen.textContent=currentScreen.textContent.slice(0, currentScreen.textContent.length - 1);
+}
+
+
+
+
+
+
+
+
+
 
 
 
